0bd59726213a522d69e1c9733bb3bcb36edea77f
Jean Fraga <jeanfragoli395@gmail.com>
2023-07-31 16:52:57 -0300

    style refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import GlobalStyle from './components/global-style';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { GlobalStyle } from './components/global-style'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
-);
-reportWebVitals();
+  </React.StrictMode>,
+)
+reportWebVitals()
